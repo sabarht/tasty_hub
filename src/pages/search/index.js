c0d840c3fa56139a4ca3b8ae7ca9490c69f60505
Saba <sabarht@gmail.com>
2023-07-21 15:41:32 +0200

    creating search page

---
+++ src/pages/search/index.js
@@ -0,0 +1,29 @@
+import SearchForm from "../../../components/searchForm/searchForm";
+import { useState } from "react";
+import useSWR from "swr";
+import RecipeList from "../../../components/recipesList/recipeList";
+
+export default function SearchPage() {
+  const { data } = useSWR("/api/recipes");
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchValue, setSearchValue] = useState(" ");
+
+  function handleSearchResults(e) {
+    e.preventDefault();
+    setSearchValue(e.target.value);
+    console.log("search", searchValue);
+    if (data) {
+      const filteredResults = data.filter((recipe) =>
+        recipe.title.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      console.log("filter", filteredResults);
+      setSearchResults(filteredResults);
+    }
+  }
+  return (
+    <>
+      <SearchForm onChange={handleSearchResults} />
+      <RecipeList data={searchResults} />
+    </>
+  );
+}
