f6494d2ce052fb0d3fef23bd29c77244ddf241d0
Saba <sabarht@gmail.com>
2023-07-23 11:42:09 +0200

    adding navigation to every page

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function Navigation() {
   return (
-    <ul className="flex justify-around bg-red-600 ">
+    <ul className="flex justify-around p-2 bg-red-600 ">
       <li>
         <Link href="/">Home</Link>
       </li>
